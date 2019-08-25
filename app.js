c8130b2ab4b16fbc9f2bc441e482e2e581e5024d
tfsn <tfsn86@gmail.com>
2019-08-25 21:14:02 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,44 @@
+// Require express
 const express = require('express');
-const { data } = require('../data/data.json');
 
+// Require projectdata from data.json
+const data = require('./data.json');
+const projects = data.projects;
+
+// Use express in app
+const app = express();
+
+// Setting view engine to pug
 app.set('view engine', 'pug');
 
+// Serves static files
 app.use('/static', express.static('public'));
+
+/**
+ * Setting our routes
+ */
+
+// An index route to render the home page
+app.get('/', (req, res) => {
+	res.render('index');
+});
+
+//Sets local variable equal to the projects listed in data.json
+app.locals = projects;
+
+// An about route to render the about page
+app.get('/about', (req, res) => {
+	res.render('about');
+});
+
+// An dynamic project route to render routes based on project id and renders a customized version of the pug project template.
+app.get('/project/:id', (req, res, next) => {
+	const { id } = req.params;
+
+	const project = projects[id];
+	res.render('project', { project });
+});
+
+app.listen(3000, () => {
+	console.log('This application is running on localhost:3000!');
+});
